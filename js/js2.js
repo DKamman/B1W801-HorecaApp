8f0564fd915b13e9125426201f0b94e019f237e0
Unknown <99053978@mydavinci.nl>
2018-11-18 14:30:07 +0100

    final commit

--- js/js2.js
+++ js/js2.js
@@ -21,14 +21,35 @@ totalBb16 	= 0;
 order = true;
 snackOrder = true;
 
-//De function: drankjes houd de hoeveelheid, en welke drankjes er besteld zijn.
+//De function: drankjes houd de hoeveelheid, en welke drankjes er besteld zijn bij.
 function drankjes(){
 	while(order == true){
-		var drank = prompt('Wat wilt u bestellen? (bier, wijn of fris)');
+		var drank = prompt('Wat wilt u bestellen? (bier, wijn, fris of snack)');
+		
 		if (drank == 'stop') {
 			order = false;
 			console.log('Order Cancelled');
+
+			//Na het invoeren van stop bij deze prompt gaat de code nog wel de else in, waarna weer een invoer gegeven moet worden
+			//voordat de code doorgaat naar de function snacks.
+			//Dit inplaats van dat de function drankjes gestopt wordt en de code door zou moeten gaan naar de function snacks
+			//na de eerste keer intypen van stop.
+			//De console logged wel "Order Cancelled", Maar gaat daarna alsnog naar: 
+			//var qty = parseInt(prompt('Hoeveel ' + drank + ' wilt u bestellen?'));
+			//Het maakt niet uit wat er in wordt gevoerd, deze function stopt dan wel alsnog en gaat door naar de function snacks.
+
 		}
+		if (drank == 'snack') {
+				order = false;
+				console.log('Order Cancelled');
+			}
+
+		/*if (drank != 'fris') {
+				alert('U hebt iets verkeerds ingetypt. Probeer het opnieuw.');
+			}*/
+		//Ik krijg deze alert niet aan de praat. Zodra ik meer conditions toevoeg, doormiddel van comparison operators
+		//of door meerdere if statements te gebruiken, krijg ik bij elke invoer, ook de goede invoer, deze alert te zien.
+
 		else{
 			var qty = parseInt(prompt('Hoeveel ' + drank + ' wilt u bestellen?'));
 			if (drank === 'fris') {
@@ -55,12 +76,6 @@ function drankjes(){
 					totalWijn = qtyWijn * WIJNPRICE
 				}
 			}
-			/*if (drank !== 'fris' || 'bier' || 'wijn' || 'stop') {
-				alert('U heeft iets verkeerds ingetypt. druk op F5 en probeer opnieuw.');
-			}*/
-
-			//Ik krijg het niet voor elkaar om een alert te laten weergeven als er eenverkeerde invoer is gedaan.
-			//Om een 1 of andere reden krijg ik de alert zodra ik de hoeveelheid van een gekozen product invoer.
 		}
 	}
 }
@@ -68,19 +83,20 @@ function drankjes(){
 //De function snacks houd bij welke order bitterballen er besteld is en hoeveel porties per order
 function snacks(){
 	while(snackOrder == true){
-		var snack = prompt('Wat wilt u bestellen? (snack) (meerdere opties komen nog)');
-		if (snack == 'stop') {
+		var bbQty = prompt('Hoeveel bitterballen wilt u bestellen? (8 of 16)');
+		if (bbQty == 'stop') {
 			snackOrder = false;
 			console.log('Order Cancelled');
 		}
 		else{
-			var bbQty = parseInt(prompt('Hoeveel bitterballen wilt u bestellen? (8 of 16)'));
 			console.log(bbQty);
+
 			/*if (bbQty != 8 && 16) {
 				alert('U kunt alleen een keuze maken tussen 8 en 16. De snacks zijn niet toegevoegd aan de bestelling.');
 			}*/
 			//Ook hier krijg ik de alert niet voor elkaar.
-			if (bbQty === 8) {
+
+			if (bbQty == 8) {
 				var bbPortie8 = parseInt(prompt('Hoeveel bitterbalschalen van aantal stuks wilt u bestellen?'));
 				bbQty8 = bbQty8 + bbPortie8
 				console.log(bbPortie8);
@@ -90,7 +106,7 @@ function snacks(){
 					console.log(totalBb8);
 				}	
 			}
-			if (bbQty === 16) {
+			if (bbQty == 16) {
 				var bbPortie16 = parseInt(prompt('Hoeveel bitterbalschalen van aantal stuks wilt u bestellen?'));
 				bbQty16 = bbQty16 + bbPortie16
 				console.log(bbPortie16);
@@ -105,34 +121,39 @@ function snacks(){
 	}
 }
 
+//deze function rekent de totale bedragen uit van alle bestelde producten.
+//Is er niks besteld staat er "U hebt geen drankjes besteld" of "U hebt geen snacks besteld"
 function calculate(){
 	var drinkSubTotal = totalBier + totalWijn + totalFris;
 	var snackSubTotal = totalBb8 + totalBb16;
 	var totalFull = snackSubTotal + drinkSubTotal; 
 	document.write('<h2>Rekening</h2><h4>Drankjes:</h4>')
-	/*document.write('<h3>Rekening</h3><br><h4>Drankjes:</h4><p>' + qtyBier + ' bier ' + '€'+ totalBier.toFixed(2) + '<br>'  + qtyWijn + ' wijn ' + '€'+ totalWijn.toFixed(2) + '<br>' + qtyFris + ' fris ' + '€'+ totalFris.toFixed(2) + '<br>' + 'Subtotaal: €' + drinkSubTotal.toFixed(2) + '</p><br><br>');*/
-	if (qtyBier > 0) {
-		document.write(qtyBier + " x " + bierName + "\u00A0 " + '€' + totalBier.toFixed(2) + '<br>');
+	if (qtyBier || qtyWijn || qtyFris > 0) {	
+		if (qtyBier > 0) {
+			document.write(qtyBier + " x " + bierName + "\u00A0 " + '€' + totalBier.toFixed(2) + '<br>');
+		} 
 		if (qtyWijn > 0) {
 			document.write(qtyWijn + " x " + wijnName + "\u00A0 " + '€' + totalWijn.toFixed(2) + '<br>');
-			if (qtyFris > 0) {
-				document.write(qtyFris + " x " + frisName + "\u00A0 " + '€' + totalFris.toFixed(2) + '<br>');
+		}
+		if (qtyFris > 0) {
+			document.write(qtyFris + " x " + frisName + "\u00A0 " + '€' + totalFris.toFixed(2) + '<br>');
 		}
 	}
-}
 	else{
-		document.write('U heeft geen drankjes besteld.<br>')
+		document.write('U hebt geen drankjes besteld.<br>')
 	}
 	document.write('Subtotaal: ' + '€' + drinkSubTotal.toFixed(2));
 	document.write('<h4>Snacks:</h4>')
-	if (bbQty8 > 0) {
-		document.write(bbQty8 + " x " + bbName8 + "\u00A0 " + '€' + totalBb8.toFixed(2) + '<br>');
+	if (bbQty8 || bbQty16 > 0) {	
+		if (bbQty8 > 0) {
+			document.write(bbQty8 + " x " + bbName8 + "\u00A0 " + '€' + totalBb8.toFixed(2) + '<br>');
+		} 
 		if (bbQty16 > 0) {
 			document.write(bbQty16 + " x " + bbName16 + "\u00A0 " + '€' + totalBb16.toFixed(2) + '<br>');
-	}
+		}
 }
 	else{
-		document.write('U heeft geen snacks besteld.<br>')
+		document.write('U hebt geen snacks besteld.<br>')
 	}
 	document.write('Subtotaal: ' + '€' + snackSubTotal.toFixed(2) + '<br><br>');
 	document.write('<h3>Het totaal te betalen bedrag is: €' + totalFull.toFixed(2) + '</h3>');
